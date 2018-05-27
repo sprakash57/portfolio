bc3261df66fe3a720e4fcd44e326add8fca3f442
Sunny Prakash <sunny.prakashgm@gmail.com>
2018-05-28 00:02:16 +0530

    added about section and Skill section.

---
+++ js/script.js
@@ -0,0 +1,46 @@
+$(document).ready(function(){
+    $('#slides').superslides({
+        animation: 'fade',
+        play: 3000,
+        pagination: false
+    });
+
+    var typed = new Typed(".typed",{
+        strings: ["Web Developer.","Android developer.","Student.","Anime lover."],
+        typeSpeed: 80,
+        loop: true,
+        startDelay: 1000,
+        showCursor: false
+    });
+
+    $('.owl-carousel').owlCarousel({
+        loop:true,
+        items: 4,
+        responsive:{
+            0:{
+                items:1
+            },
+            480:{
+                items:2
+            },
+            768:{
+                items:3
+            },
+            938:{
+                items:4
+            }
+        }
+    });
+
+    $('.chart').easyPieChart({
+        easing: 'easeInOut',
+        barColor: '#fff',
+        trackColor: false,
+        scaleColor: false,
+        lineWidth: 4,
+        size: 152,
+        onStep: function(from, to, percent){
+            $(this.el).find('.percent').text(Math.round(percent));
+        }
+    });
+});
