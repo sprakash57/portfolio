5a993a5ac231dd598d357625850ac729471f6257
Sunny Prakash <sunny.prakashgm@gmail.com>
2019-04-07 14:08:34 +0530

    Updates

---
+++ precache-manifest.b9749d96984316294ee9733908d8bfe4.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = [
+  {
+    "revision": "8df933bc30db3790d414936e563a96ff",
+    "url": "/Portfolio/static/media/welcome.8df933bc.jpg"
+  },
+  {
+    "revision": "23d2d723f29fdc63fc2714a74825789d",
+    "url": "/Portfolio/static/media/gslab.23d2d723.jpg"
+  },
+  {
+    "revision": "d11a7f089efe6e22f0d72d3af903f1f1",
+    "url": "/Portfolio/static/media/cogni.d11a7f08.png"
+  },
+  {
+    "revision": "edbc89f9063aa4528aa4b4bfcd6a8f12",
+    "url": "/Portfolio/static/media/about.edbc89f9.jpg"
+  },
+  {
+    "revision": "6b64d540f1308ee928ad",
+    "url": "/Portfolio/static/js/runtime~main.132fbc8c.js"
+  },
+  {
+    "revision": "179cd3a52c435f98fa96",
+    "url": "/Portfolio/static/js/main.5524047e.chunk.js"
+  },
+  {
+    "revision": "ccd950b57bd474e0eb02",
+    "url": "/Portfolio/static/js/2.54fe09a8.chunk.js"
+  },
+  {
+    "revision": "179cd3a52c435f98fa96",
+    "url": "/Portfolio/static/css/main.e4fb7455.chunk.css"
+  },
+  {
+    "revision": "d2b690a6ee0f81bbbbcb8284669b6088",
+    "url": "/Portfolio/index.html"
+  }
+];
