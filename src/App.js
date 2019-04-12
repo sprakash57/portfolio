bb5e39e2f30f89c7c93403dd951d36bec2c10302
Sunny Prakash <sunny.prakashgm@gmail.com>
2019-04-12 12:54:49 +0530

    ROUTES: routes transition implemented

--- src/App.js
+++ src/App.js
@@ -1,29 +1,47 @@
 import React, { Component } from 'react';
 import Navigation from './Screens/Navigation';
 import Welcome from './Screens/Welcome';
-import {HashRouter, Switch, Route} from 'react-router-dom';
+import {HashRouter, Route} from 'react-router-dom';
+import { Container } from 'react-bootstrap';
 import Contact from './Screens/Contact/Contact';
 import Stats from './Screens/Stats/Stats';
 import Portfolio from './Screens/Portfolio/Portfolio';
 import About from './Screens/About/About';
+import { CSSTransition } from 'react-transition-group';
 
 class App extends Component {
-  constructor(props){
-    super(props);
-  }
   render() {
     const { env } = this.props;
+    const routes = [
+      { path: '/', name: 'Welcome', Component: Welcome },
+      { path: '/about', name: 'About', Component: About },
+      { path: '/stats', name: 'Stats', Component: Stats },
+      { path: '/portfolio', name: 'Portfolio', Component: Portfolio },
+      { path: '/contact', name: 'Contact', Component: (props) => <Contact {...props} env={env}/> }
+    ]
+    
     return (
       <HashRouter>
         <React.Fragment>
           <Navigation/>
-          <Switch>
-            <Route exact path="/" component={Welcome}/>
-            <Route exact path="/about" component={About}/>
-            <Route exact path="/contact" render={(props) => <Contact {...props} env={env}/>}/>
-            <Route exact path="/stats" component={Stats}/>
-            <Route exact path="/portfolio" component={Portfolio}/>
-          </Switch>
+          <Container className="container">
+          {routes.map(({ path, Component }) => (
+            <Route key={path} exact path={path}>
+              {({ match }) => (
+                <CSSTransition
+                  in={match != null}
+                  timeout={300}
+                  classNames="page"
+                  unmountOnExit
+                >
+                  <div className="page">
+                    <Component />
+                  </div>
+                </CSSTransition>
+              )}
+            </Route>
+          ))}
+        </Container>
         </React.Fragment>
       </HashRouter>
     );
