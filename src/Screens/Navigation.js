37d94f5816710a8e29d2736c8174358b254e74b1
Sunny Prakash <sunny.prakashgm@gmail.com>
2019-04-12 18:28:03 +0530

    WELCOME: page completed

--- src/Screens/Navigation.js
+++ src/Screens/Navigation.js
@@ -1,16 +1,19 @@
 import React from 'react';
-import {Navbar} from 'react-bootstrap';
+import {Navbar, Nav} from 'react-bootstrap';
 import {Link} from 'react-router-dom';
+
 
 const Navigation = () => (
     <Navbar className="navigation" expand="lg" fixed="top">
-        <Link className="text-design" to="/">Hire.me</Link>
+        <Link to="/" style={{fontSize: 40}}>SU:PR</Link>
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
-        <Navbar.Collapse id="basic-navbar-nav" className="justify-content-end">
-            <Link className="text-design" to="/about">About</Link>
-            <Link className="text-design" to="/stats">Stats</Link>
-            <Link className="text-design" to="/portfolio">Portfolio</Link>
-            <Link className="text-design" to="/contact">Contact</Link>      
+        <Navbar.Collapse id="basic-navbar-nav">
+            <Nav className="ml-auto justify-content-end">
+                <Link to="/about">About</Link>
+                <Link to="/stats">Stats</Link>
+                <Link to="/portfolio">Portfolio</Link>
+                <Link to="/contact">Contact</Link>    
+            </Nav>  
         </Navbar.Collapse>
     </Navbar>
 );
--- src/Screens/Welcome.js
+++ src/Screens/Welcome.js
@@ -1,26 +1,49 @@
 import React from 'react';
 import Typed from 'react-typed';
 import {Container, Col, Row} from 'react-bootstrap';
+import cx from 'classnames';
+
+  const getTime = () => {
+    let hrs = new Date().getHours();
+    console.log(hrs);
+    switch(true) {
+      case (hrs < 12):
+        return {"greeting":"Good Morning!!", "bgStyle": "morning"}
+      case (hrs >= 12 && hrs < 16):
+        return {"greeting":"Good Afternoon!!", "bgStyle": "noon"}
+      case (hrs >= 16 && hrs < 22):
+        return {"greeting":"Good Evening!!", "bgStyle": "evening"}
+      default:
+        return {"greeting":"Good Evening!!", "bgStyle": "night"}
+    }
+  }
+
    
   const Welcome = () => (
       <Container>
         <Row>
           <Col xs={12}>
+          <div className={cx(getTime().bgStyle, 'time')}>
+              <h3>{getTime().greeting}</h3>
+          </div>
             <div className="content">
               <h1>SUNNY PRAKASH</h1>
               <Typed
                 className="usp"
-                strings={['Android Dev','Web development','Anime freak','Learning never stops']} 
+                strings={['Android Dev','Web developer','Anime freak','Learning everyday']} 
                 typeSpeed={65}
                 backSpeed={40}
                 loop
               />
-            </div>
-          </Col>  
+              <div style={{paddingTop: 80}}>
+                <a 
+                  className="resumeBtn" 
+                  href="https://drive.google.com/open?id=1nfWrLujpIlUHXG2bi3nxOB7q_lIdCPt0"
+                  target="_blank">My Resume</a>
+              </div>
+            </div>            
+          </Col>           
         </Row>  
-        <div>
-          <a className="resumeBtn" href="#">My Resume</a> 
-        </div>
       </Container>
   );
 
