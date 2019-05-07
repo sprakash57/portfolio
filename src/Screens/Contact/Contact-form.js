5789e188b98c3be0541f1489bab949f703c3799d
Sunny Prakash <sunny.prakashgm@gmail.com>
2019-05-07 22:23:37 +0530

    PR-16: Fixed form validation

--- src/Screens/Contact/Contact-form.js
+++ src/Screens/Contact/Contact-form.js
@@ -1,10 +1,9 @@
 import React from 'react';
 import { Col, Form } from 'react-bootstrap';
-import cx from 'classnames';
 import './Contact.css';
 
 const phoneRegex = RegExp(/^\s*(?:\+?(\d{1,3}))?[-. (]*(\d{3})[-. )]*(\d{3})[-. ]*(\d{4})(?: *x(\d+))?\s*$/);
-const emailRegex = RegExp(/^[a-zA-Z0-9.!#$%&’*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/);
+const emailRegex = RegExp(/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/);
 
 class ContactForm extends React.Component{
     constructor(){
@@ -15,7 +14,6 @@ class ContactForm extends React.Component{
             phone: "",
             feedback: "",
             formSubmitted: false,
-            valid: true,
             formErrors: {
                 email: "",
                 phone: ""
@@ -23,84 +21,58 @@ class ContactForm extends React.Component{
         }
     }
 
-    formValid = ({formErrors, email, phone}) => {
+    hasValidFormData = ({formErrors, email, phone}) => {
         let valid = true;
         Object.values(formErrors).forEach(val => val.length > 0 && (valid = false));
         if(email === "" && phone === "") {
             valid = false;
         }
-        console.log(valid);
         return valid;
-    }
-
-    //static sender = "12prakash.sunny@gmail.com";
-
-    handleFeedback = (event) => {
-        this.setState({
-            ...this.state,
-            feedback:event.target.value
-        })
-    }
-
-    handleName = (event) => {
-        this.setState({
-            ...this.state,
-            name:event.target.value
-        })
     }
 
     handleValidation = e => {
         e.preventDefault();
         const { name, value } = e.target;
-        console.log(name, value);
         let formErrors = {...this.state.formErrors};
         switch(name) {
-            case "Email":
-                formErrors.email = emailRegex.test(value) ? "" : "Email is not valid";
+            case "email":
+                formErrors.email = emailRegex.test(value) ? "" : "Please enter a valid email id";
                 break;
-            case "Phone":
+            case "phone":
                 formErrors.phone = phoneRegex.test(value) ? "" : "Phone number is not valid";
                 break;
             default:
                 break;
         }
 
-        this.setState({formErrors, [name]: value}, () => console.log("validattion data", this.state));
-    }
-
-    handleBtnAnimation = () => {
-        let btn = document.getElementById('btn');
-        btn.classList.remove("shake-btn");
+        this.setState({formErrors, [name]: value});
     }
 
     handleSubmit = (event) => {
         event.preventDefault();
-        if(this.formValid(this.state)) {
-            const {email, phone} = this.state;
-            console.log("success", email, phone);
+        if(this.hasValidFormData(this.state)) {
+            console.log("success");
+            const {
+                REACT_APP_EMAILJS_RECEIVER: receiverEmail,
+                REACT_APP_EMAILJS_TEMPLATEID: template
+            } = this.props.env;
+        
+            this.sendFeedback(
+                template,
+                receiverEmail,
+                this.state.name,
+                this.state.email,
+                this.state.phone,
+                this.state.feedback
+            );
+        
+            this.setState({
+              formSubmitted: true
+            });
         } else {
-            console.log("failure");
-            this.setState({...this.state, valid: false});
-            this.handleBtnAnimation();
+            //to do stuff
+            console.log("Failed to validate form data");
         }
-        // const {
-        //     REACT_APP_EMAILJS_RECEIVER: receiverEmail,
-        //     REACT_APP_EMAILJS_TEMPLATEID: template
-        // } = this.props.env;
-    
-        // this.sendFeedback(
-        //     template,
-        // //   this.sender,
-        //     receiverEmail,
-        //     this.state.name,
-        //     this.state.email,
-        //     this.state.phone,
-        //     this.state.feedback
-        // );
-    
-        // this.setState({
-        //   formSubmitted: true
-        // });
     }
 
     sendFeedback = (templateId, receiverEmail, name, phone, email, feedback) => {
@@ -126,55 +98,55 @@ class ContactForm extends React.Component{
     }
 
     render(){
-        const {formErrors, valid} =  this.state;
-        console.log('render', valid);
+        const {formErrors} =  this.state;
         return (
                 <Col md={6}>
                     <div className="form-title">
                         <h1>GET IN TOUCH</h1>
                     </div>
                     <Form onSubmit={this.handleSubmit} noValidate>
-                        <Form.Group controlId="formBasicEmail">
+                        <Form.Group>
                             <Form.Control 
-                                type="text" 
-                                value={this.state.name} 
+                                type="text"
+                                name="name"
                                 placeholder="Name/Company" 
-                                onChange={this.handleName}
+                                onChange={this.handleValidation}
                                 />
                         </Form.Group>
-                        <Form.Group>
+                        <Form.Group controlId="formBasicEmail">
                             <Form.Control
                                 className={formErrors.email.length > 0 ? "error" : null}
                                 type="email"
-                                name="Email"
+                                name="email"
                                 placeholder="Email*" 
                                 onChange={this.handleValidation}
                                 noValidate
                                 />
-                            {!valid && (
-                                <span className="errorMsg">{formErrors.email}</span>
+                            {formErrors.email.length > 0 && (
+                                <small className="errorMsg">{formErrors.email}</small>
                             )}
                         </Form.Group>
                         <Form.Group>
                             <Form.Control
                                 className={formErrors.phone.length > 0 ? "error" : null}
-                                name="Phone"
+                                name="phone"
                                 placeholder="Phone*"
                                 onChange={this.handleValidation}
                                 />
-                            {!valid && (
-                                <span className="errorMsg">{formErrors.phone}</span>
+                            {formErrors.phone.length > 0 && (
+                                <small className="errorMsg">{formErrors.phone}</small>
                             )}
                         </Form.Group>
                         <Form.Group controlId="exampleForm.ControlTextarea1">
                             <Form.Control 
                                 as="textarea"
                                 rows="6"
+                                name="feedback"
                                 placeholder="Feedback/message"
-                                onChange={this.handleFeedback} 
-                                value={this.state.feedback}/>
+                                onChange={this.handleValidation}
+                                />
                         </Form.Group>
-                        <button id="btn" className={cx("send-btn")} type="submit">Send</button>
+                        <button id="btn" className="send-btn" type="submit">Send</button>
                     </Form>
                 </Col>
             );
--- src/Screens/Portfolio/Portfolio-api.js
+++ src/Screens/Portfolio/Portfolio-api.js
@@ -34,7 +34,7 @@ const workDone = [
     {
         "id": 1,
         "url": "https://github.com/sprakash57/todo-app",
-        "title": "Redux Tasker",
+        "title": "Tasker",
         "classes": "sticky-l",
         "background": "#cfc",
         "tech":"Web",
@@ -51,51 +51,58 @@ const workDone = [
         "background": "#e380bc",
         "tech":"Android",
         "stack": [androidOs, java],
-        "body": "lorem ipsum",
+        "body": `My first official android app. Coded in java, Connect 3 is a 3x3 classic Tic Tac Toe board game. The player who 
+                succeeds in placing three of their marks in a horizontal, vertical, or diagonal row wins the game. You can play with your
+                bestie or try to challange your smartphone in single player mode`,
         "image": `https://img.icons8.com/dusk/${imageSize}/000000/share-2.png`
     },
     {
         "id": 3,
         "url": "https://github.com/sprakash57/What-s-the-weather",
-        "title": "What's the weather?",
+        "title": "Weather today?",
         "classes": "sticky-l",
         "background": "#ccf",
         "tech":"Web",
         "stack": [react, javascript, weatherIcon],
-        "body": "lorem ipsum",
+        "body": `Weather today gives you flexibility to access forecast information of different cities in a list format. 
+                Just search for your city name and it will provide you sunset and sunrise time, weather info, max and min temperature. 
+                It is using OpenWeather API in the backend.`,
         "image": `https://img.icons8.com/bubbles/${imageSize}/000000/sun.png`
     },
     {
         "id": 4,
         "url": "https://github.com/sprakash57/book-explorer",
-        "title": "Book-explorer",
+        "title": "Bookplorer",
         "classes": "sticky-r",
         "background": "#e380bc",
         "tech":"Web",
         "stack": [react, javascript, googleIcon],
-        "body": "lorem ipsum",
+        "body": `Google has many book in their database and this app let you tap into that treasure. You put the title of the book and it
+                 provide you all the possible outcome in galary format.`,
         "image": `https://img.icons8.com/clouds/${imageSize}/000000/book.png`
     },
     {
         "id": 5,
         "url": "https://github.com/sprakash57/User-Search",
-        "title": "gitHub user search",
+        "title": "gitHub users",
         "classes": "sticky-r",
         "background": "#bf8",
         "tech":"Web",
         "stack": [angularIcon, typescriptIcon, githubOctocat],
-        "body": "lorem ipsum",
+        "body": `This was developed as a POC project after learning Angualar 6 and typescript. It searches all the github users for you 
+                along with some useful data.`,
         "image": `https://img.icons8.com/color/${imageSize}/000000/github.png`
     },
     {
         "id": 6,
         "url": "https://github.com/sprakash57/web-customer-tracker",
-        "title": "Customer-tracker",
+        "title": "CRM",
         "classes": "sticky-l",
         "background": "#ffc",
         "tech":"Web",
         "stack": [java, spring, bootstrap, hibernate],
-        "body": "lorem ipsum",
+        "body": `Built on Spring framework CRM(Customer relationship manager) app presents a miniature version of salesforce. It has its 
+                database where customer data can be stored, retrieved, updated and deleted`,
         "image": `https://img.icons8.com/cotton/${imageSize}/000000/customer-support.png`
     },
     {
@@ -106,7 +113,8 @@ const workDone = [
         "background": "#f3a456",
         "tech":"Web",
         "stack": [react, reactRouter, bootstrap],
-        "body": "lorem ipsum",
+        "body": `This is the very website where you are browsing right now. Initially it was based on vanilla javascript. But later on 
+                developed using React and some other latest javascript libraries.`,
         "image": `https://img.icons8.com/office/${imageSize}/000000/set-as-resume.png`
     }
 ];
