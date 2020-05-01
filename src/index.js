d2bfa636f0cc8178b643a1d21cb5c9da508abd68
Sunny Prakash <sunny.prakashgm@gmail.com>
2020-05-02 00:12:41 +0530

    initial setup for the app

--- src/index.js
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import { env } from './Config';
-
-ReactDOM.render(<App env={env}/>, document.getElementById('root'));
