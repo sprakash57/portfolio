0628a1a36ec78abd988cb7d33205dcd1d577206b
Sunny Prakash <sunny.prakashgm@gmail.com>
2021-07-24 11:17:28 +0530

    chore: initial cleanup

--- src/components/badge.js
+++
@@ -1,6 +0,0 @@
-import React from 'react';
-import styles from './badge.module.scss';
-
-const Badge = ({ tag }) => <span className={styles.badge}>{tag.toLowerCase()}</span>;
-
-export default Badge;
--- src/components/pop.js
+++
@@ -1,5 +0,0 @@
-import React from 'react';
-
-const Pop = ({ text, size }) => <span style={{ color: 'tomato', fontSize: size }}>{text}</span>;
-
-export default Pop;
