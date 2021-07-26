870897476cda07ba5150b6e56c49f0c93b6a7ef2
Sunny Prakash <sunny.prakashgm@gmail.com>
2021-07-26 23:11:03 +0530

    refactor: Worked on blog card

--- components/Blog/BlogCard.tsx
+++ components/Blog/BlogCard.tsx
@@ -5,10 +5,10 @@ import RouteLink from '../Common/RouteLink';
 const BlogCard = ({ post }: { post: Post }) => {
     const { slug, title, summary, publishedAt } = post;
     return (
-        <RouteLink href={`blog/${slug}`} passHref>
+        <RouteLink href={`/blogs/${slug}`} passHref>
             <section key={title} className={styles.card}>
                 <h3>{title}</h3>
-                <p>{summary}</p>
+                <summary>{summary}</summary>
                 <small>{publishedAt}</small>
             </section>
         </RouteLink>
--- pages/index.tsx
+++ pages/index.tsx
@@ -2,34 +2,42 @@ import { useMemo } from "react";
 import BlogCard from "../components/Blog/BlogCard";
 import { getAllFilesFrontMatter } from "../helpers/mdx";
 import { Post } from "../types";
+import styles from "../styles/Home.module.scss";
+import RouteLink from "../components/Common/RouteLink";
 
 const Home = ({ posts }: { posts: Post[] }) => {
+    console.log(posts)
+    const recentPosts = useMemo(() => {
+        return posts
+            .sort((prevPost, nextPost) => new Date(nextPost.publishedAt).getTime() - new Date(prevPost.publishedAt).getTime())
+            .slice(0, 3)
+    }, [posts])
 
-  const recentPosts = useMemo(() => {
-    return posts
-      .sort((prevPost, nextPost) => new Date(nextPost.publishedAt).getTime() - new Date(prevPost.publishedAt).getTime())
-      .slice(0, 3)
-  }, [posts])
-
-  return (
-    <section>
-      <article>
-        <h2>Recent Blogs</h2>
-        {recentPosts.map(post => <BlogCard key={post.title} post={post} />)}
-      </article>
-      <article>
-        <h2>Featured Projects</h2>
-      </article>
-      <article>
-        <h2>What&apos;s cooking</h2>
-      </article>
-    </section>
-  )
+    return (
+        <section>
+            <article className={styles.agenda}>
+                <h1>Recent Blogs</h1>
+                {recentPosts.map(post => <BlogCard key={post.title} post={post} />)}
+                <RouteLink
+                    href="/blogs"
+                    classForContainer={styles.viewMore}
+                >
+                    View more
+                </RouteLink>
+            </article>
+            <article className={styles.agenda}>
+                <h1>Featured Projects</h1>
+            </article>
+            <article className={styles.agenda}>
+                <h1>What&apos;s cooking</h1>
+            </article>
+        </section>
+    )
 }
 
 export default Home;
 
 export async function getStaticProps() {
-  const posts = await getAllFilesFrontMatter('posts')
-  return { props: { posts } }
+    const posts = await getAllFilesFrontMatter('posts')
+    return { props: { posts } }
 }
