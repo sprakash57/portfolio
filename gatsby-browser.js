37cb3479e52a31676b6e51bc9023bfeb354e486a
Sunny Prakash <sunny.prakashgm@gmail.com>
2021-08-15 21:37:39 +0530

    chore: Master cleanup

--- gatsby-browser.js
+++
@@ -1,5 +0,0 @@
-import 'bootstrap/dist/css/bootstrap.css';
-import 'jquery/dist/jquery.slim.min.js';
-import 'popper.js/dist/popper.min';
-import 'bootstrap/dist/js/bootstrap.min.js';
-import "typeface-bangers";
