cfe6cb12db5af5dc499545a9f9da55cf9eb60c36
Sunny Prakash <sunny.prakashgm@gmail.com>
2021-08-15 20:44:12 +0530

    style: minor changes to the navbar

--- components/NavMenu.tsx
+++ components/NavMenu.tsx
@@ -2,19 +2,34 @@ import { RouteLink } from "@/common/index";
 import styles from '../styles/components/NavMenu.module.scss';
 import Image from "next/dist/client/image";
 import Brand from '@/public/brand.svg';
+import React from "react";
 
 const NavMenu = () => {
+    const setActive = (e: React.MouseEvent<HTMLDivElement>) => {
+        const target = e.target as HTMLDivElement;
+        const closestEl = target.closest("a");
+        if (target.id.includes("menu-") && closestEl) {
+            closestEl.parentElement?.childNodes.forEach((child: any) => {
+                if (child.id === closestEl.id) {
+                    child.classList.add(styles.active);
+                } else {
+                    child.classList.remove(styles.active);
+                }
+            });
+            console.dir(closestEl.parentElement?.childNodes);
+        }
+    }
     return (
         <nav className={styles.nav}>
             <RouteLink href="/" classForContainer={styles.navBrand}>
                 <Image src={Brand} alt="Brand" />
             </RouteLink>
-            <section className={styles.navMenu}>
-                <RouteLink href="/">/</RouteLink>
-                <RouteLink href="/blogs">Blogs</RouteLink>
-                <RouteLink href="/projects">Projects</RouteLink>
-                <RouteLink href="/about">About</RouteLink>
-            </section>
+            <div className={styles.navMenu} role="Button" onClick={setActive}>
+                <RouteLink href="/" childProps={{ id: "menu-home" }} classForContainer={styles.active}>Home</RouteLink>
+                <RouteLink href="/blogs" childProps={{ id: "menu-blogs" }}>Blogs</RouteLink>
+                <RouteLink href="/projects" childProps={{ id: "menu-projects" }}>Projects</RouteLink>
+                <RouteLink href="/about" childProps={{ id: "menu-about" }}>About</RouteLink>
+            </div>
         </nav>
     )
 }
