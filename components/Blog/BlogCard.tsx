276fa8c11eb0724e783a52acfd2bc17eb1b39854
Sunny Prakash <sunny.prakashgm@gmail.com>
2021-08-15 22:01:45 +0530

    fix: Added proper card type

--- components/Blog/BlogCard.tsx
+++ components/Blog/BlogCard.tsx
@@ -1,4 +1,4 @@
-import Card from "../common/Card";
+import { Card } from "@/components/common";
 import NextImage from "next/image";
 import styles from "@/styles/components/BlogCard.module.scss";
 import ExtLink from "@/public/icons/external.svg";
