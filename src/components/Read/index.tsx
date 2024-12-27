17defef441bcd314aa63344e462b5ceba45708ba
Sunny Prakash <sunny.prakashgm@gmail.com>
2024-12-27 10:57:44 +0100

    Update blogs

--- src/components/Read/index.tsx
+++ src/components/Read/index.tsx
@@ -7,8 +7,8 @@ import { AnimateLink, VerticalDivider } from 'components/common/styled';
 const blogPosts = [
   {
     id: '3',
-    url: 'https://medium.com/javascript-in-plain-english/efficient-thumbnail-generation-for-pdf-more-in-express-js-2a883bde4d25',
-    title: 'Efficient thumbnail generation for PDFs & more in Express.js',
+    url: 'https://medium.com/@sunny-prakash/efficient-thumbnail-generation-for-pdf-more-in-express-js-2a883bde4d25',
+    title: 'How to generate Thumbnails for PDF & more in Express.js?',
   },
   {
     id: '2',
@@ -17,13 +17,13 @@ const blogPosts = [
   },
   {
     id: '1',
-    url: 'https://medium.com/javascript-in-plain-english/the-secret-to-debugging-your-live-react-native-android-app-34c703fbbf87',
-    title: 'The Secret to Debugging Your Live React Native Android App',
+    url: 'https://javascript.plainenglish.io/the-secret-to-debugging-your-live-react-native-android-app-34c703fbbf87',
+    title: 'Checking Logs for React Native Apps in Production',
   },
   {
     id: '4',
     url: 'https://medium.com/javascript-in-plain-english/how-to-use-environment-variables-in-github-actions-workflows-598b583c1a92',
-    title: 'How to use use Environment variables in Github Actions workflow',
+    title: 'How to use use Environment variables in Github Actions workflow?',
   },
 ];
 
